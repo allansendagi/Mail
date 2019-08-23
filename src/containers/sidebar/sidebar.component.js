2c947a259ac6f295cfacd611a0de6731eec0a269
allansendagi <sendagiallan87@gmail.com>
2019-08-24 00:50:02 +0400

    finishing side bar specs

--- src/containers/sidebar/sidebar.component.js
+++ src/containers/sidebar/sidebar.component.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import SideBarItem from './sidebaritem/sidebaritem.component';
 import SideBarHeader from './sidebarheader/sidebar-header.component';
+import { SideBarFooter } from './sidebar-footer/sidebar-footer.component';
+import { Subscriptions } from './subscriptions/subscriptions.component';
+
 import { Menu, Divider } from 'semantic-ui-react';
 import './sidebar.styles.scss';
 
@@ -18,6 +21,7 @@ class SideBar extends React.Component {
 		      <SideBarItem label='Watch later' icon='clock'/>
 		      <SideBarItem label='Liked videos' icon='thumbs up'/>
 		      <Divider/>
+		      <Subscriptions />
 		      <SideBarHeader title='More from Youtube'/>
 		      <SideBarItem label='Movies and Shows' icon='film'/>
 		      <Divider/>
@@ -25,6 +29,7 @@ class SideBar extends React.Component {
 		      <SideBarItem label='Help' icon='help circle'/>
 		      <SideBarItem label='Send feedback' icon='comment'/>
 		      <Divider/>
+		      <SideBarFooter/>
 			</Menu>
 		);
 	}
