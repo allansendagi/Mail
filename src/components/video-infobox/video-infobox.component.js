e50403dd1513de8f74ca3fd318d703fa45ecca74
allansendagi <sendagiallan87@gmail.com>
2019-09-14 01:30:24 +0400

    adding the video info component

---
+++ src/components/video-infobox/video-infobox.component.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './video-infobox.styles.scss';
+import { Image, Button } from 'semantic-ui-react';
+
+export class VideoInfoBox extends React.Component {
+	render() {
+		return (
+			<div className='video-infobox'>
+			<Image className='channel-image' src='http://via.placeholder.com/48x48' circular/>
+			  <div className="video-info">
+			    <div className='channel-name'>Channel Name</div>
+			    <div className='video-publication-date'>Thu 24, 2017</div>
+			     </div>
+			      <Button className="button" color='youtube'>91.5K Subscribe</Button>
+			      <div className="video-description">
+			      <p>Paragraph 1</p>
+			      <p>Paragraph 2</p>
+			      <p>Paragraph 3</p>
+			      <p>Paragraph 4</p>
+			     <p>Paragraph 5</p>
+			    <Button compact >Show More</Button>
+			  </div>
+			</div>
+		)
+	}
+}
--- src/containers/watch/watch.component.js
+++ src/containers/watch/watch.component.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {RelatedVideos} from '../../components/related-videos/related-videos.component';
 import {Video} from '../../components/video/video.component';
 import {VideoMetadata} from '../../components/video-metadata/video-metadata.component';
+import { VideoInfoBox } from '../../components/video-infobox/video-infobox.component'
 import './watch.styles.scss';
 
 export class Watch extends React.Component {
@@ -10,6 +11,7 @@ export class Watch extends React.Component {
 			<div className='watch-grid'>
 			  <Video className='video' id='-7fuHEEmEjs'/>
 			  <VideoMetadata className='metadata' viewCount={1000}/>
+			  <VideoInfoBox className='video-info-box'/>
 			  <div className='video-info-box' style={{width: '100%', height: '100px', background: '#BD10E0'}}>Video Info box</div>
 			  <div className='comments' style={{width: '100%', height: '100px', background: '#9013FE'}}>comments</div>
 			  <RelatedVideos className='relatedVideos'/>
