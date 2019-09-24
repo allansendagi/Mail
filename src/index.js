53370a9fcd2adc9b62c4f73ca3a01e2d469d4126
allansendagi <sendagiallan87@gmail.com>
2019-09-25 02:02:29 +0400

    setting up the Youtube Api

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import {configureStore} from './store/configureStore';
 
-
+const store = configureStore();
 
 ReactDOM.render(
+ <Provider store={store}>
   <BrowserRouter>
 	<App />
-</BrowserRouter>, document.getElementById('root'));
+</BrowserRouter>
+</Provider>, document.getElementById('root'));
 
