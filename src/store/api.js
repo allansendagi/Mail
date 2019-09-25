eecaa0e44b8faa5a01c33a343d946cef5d84be8a
allansendagi <sendagiallan87@gmail.com>
2019-09-26 00:20:28 +0400

    wiring our youtube api to our app

---
+++ src/store/api.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import {Home} from './containers/home/home.component';
+import {AppLayout} from './components/app-layout/app-layout.component';
+import {Route, Switch} from 'react-router-dom';
+import {Watch} from './containers/watch/watch.component';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {youtubeLibraryLoaded} from './store/actions/api';
+
+const API_KEY = 'AIzaSyC6phn9QVZ-AmbLI1OWrK9WMvuvnrdaQ-A';
+
+class App extends Component {
+  render() {
+    return (
+      <AppLayout>
+        <Switch>
+          <Route path="/watch" component={Watch}/>
+          <Route path="/" component={Home}/>
+        </Switch>
+      </AppLayout>
+    );
+  }
+  componentDidMount() {
+    this.loadYoutubeApi();
+  }
+
+  loadYoutubeApi() {
+    const script = document.createElement("script");
+    script.src = "https://apis.google.com/js/client.js";
+
+    script.onload = () => {
+      window.gapi.load('client', () => {
+        window.gapi.client.setApiKey(API_KEY);
+        window.gapi.client.load('youtube', 'v3', () => {
+          this.props.youtubeLibraryLoaded();
+        });
+      });
+    };
+
+    document.body.appendChild(script);
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({youtubeLibraryLoaded}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(App);
